958385efb12e10624a6f058eae32286ff7e5ef60
Astrid <astridnaivirt22@outlook.com>
2024-10-17 20:16:10 -0300

    Add configuration files and global styles

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import {Button} from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <section className="m-auto mt-4 h-[700px] w-[580px] rounded-[60px] border-[3px] border-solid border-[#acb9c5] bg-[#efefef]" />
+  );
+}
